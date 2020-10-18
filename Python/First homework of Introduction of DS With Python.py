a81659e48b1460955f062d46d67651e1998b8a93
YasirAlhejaili <70330503+YasirAlhejaili@users.noreply.github.com>
2020-10-18 21:20:55 +0300

    Add files via upload

---
+++ Python/First homework of Introduction of DS With Python.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello Everyone")
+
+
+# In[2]:
+
+
+print("My name is Yasir Alhejaili")
+
+
+# In[4]:
+
+
+print("This is our first homework of Introduction of DS With Python.")
+print("From CodingDojo ad SDA")
+
+
+# In[5]:
+
+
+5+5
+
+
+# In[6]:
+
+
+100*5
+
+
+# In[7]:
+
+
+100*5 + 5
+
+
+# In[8]:
+
+
+100/2 + 500
+
+
+# In[14]:
+
+
+print("I will upload this Jupyter notebook file to CodingDojo")
+
+
+# In[15]:
+
+
+print("Thank you All")
+
