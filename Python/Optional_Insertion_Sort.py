ce52fb4c84bc68eeda1ebf4bdf1d93f548237203
YasirAlhejaili <70330503+YasirAlhejaili@users.noreply.github.com>
2020-10-20 15:11:01 +0300

    Add files via upload

---
+++ Python/Optional_Insertion_Sort.py
@@ -0,0 +1,19 @@
+# Insertion Sort
+
+def insertionSort(arr): 
+  
+    # Traverse through 1 to len(arr) 
+    for i in range(1, len(arr)): 
+  
+        key = arr[i] 
+  
+        j = i-1
+        while j >= 0 and key < arr[j] : 
+                arr[j + 1] = arr[j] 
+                j -= 1
+        arr[j + 1] = key 
+        
+    return arr
+
+insertionSort([2,6,7,3,4,9,1])
+        
